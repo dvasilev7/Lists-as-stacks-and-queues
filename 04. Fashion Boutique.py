ff02f78f568151df310a8bc58a2c7142ac0a947b
Denis <84131568+dvasilev7@users.noreply.github.com>
2021-05-24 22:34:31 +0300

    Add files via upload

--- 04. Fashion Boutique.py
+++ 04. Fashion Boutique.py
@@ -1,2 +1,16 @@
-seq = input().split()
+stack = input().split()
 rack = int(input())
+rack_counter = 1
+
+
+new_rack = rack
+while len(stack) > 0:
+    piece = int(stack.pop())
+    if new_rack >= piece:
+        new_rack -= piece
+    else:
+        stack.append(str(piece))
+        rack_counter += 1
+        new_rack = rack
+
+print(rack_counter)
